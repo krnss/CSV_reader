720124022a57415aad98741e06a4af3cd53cde05
DESKTOP-99K5BEK\HP <khayo2000@gmail.com>
2021-03-05 22:43:01 +0200

    create

---
+++ CVS_data_reader/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CVS_data_reader.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }       
+        public bool Married { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ CVS_data_reader/Models/PersonContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CVS_data_reader.Models
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext() : base("DbConnection")
+        { }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
